cdd67afba837d6b06e5b3fba3128c79b4e5e1812
Benja Tapia <benj.tapia@duocuc.cl>
2022-04-12 21:56:03 -0400

    primer proyecto v1

---
+++ static/js/custom/form-enviar-asunto.js
@@ -0,0 +1,55 @@
+function enviar(){
+    let  run = document.getElementById("txt-run").value;
+    console.log("run : " + run);
+    
+    let nombres = document.getElementById("txt-nombres").value;
+    console.log("nombres " + nombres);
+
+    let apellido_p = document.getElementById("txt-apellido-paterno").value;
+    console.log("apellido-paterno " + apellido_p);
+
+    let apellido_m = document.getElementById("txt-apellido-materno").value;
+    console.log("apellido-materno " + apellido_m);
+
+    let email = document.getElementById("txt-email").value;
+    console.log("email " + email);
+
+    let telefono = document.getElementById("txt-telefono").value;
+    console.log("telefono " + telefono);
+
+    let producto = document.getElementById("txt-producto").value;
+    console.log("producto " + producto);
+
+    let asunto = document.getElementById("txt-asunto").value;
+    console.log("asunto " + asunto);
+}
+
+
+// function enviar(){
+//     let nombres = document.getElementById("txt-nombres").values;
+//     console.log("nombres " + nombres);
+// }
+// function enviar(){
+//     let apellido = document.getElementById("txt-apellido-paterno").values;
+//     console.log("apellido-paterno " + apellido-paterno);
+// }
+//  function enviar(){
+//      let apellido = document.getElementById("txt-apellido-materno").values;
+//      console.log("apellido-materno " + apellido-materno);
+// }
+// function enviar(){
+//     let email = document.getElementById("txt-email").values;
+//     console.log("email " + email);
+// }
+// function enviar(){
+//     let telefono = document.getElementById("txt-telefono").values;
+//     console.log("telefono " + telefono);
+// }
+// function enviar(){
+//     let producto = document.getElementById("txt-producto").values;
+//     console.log("producto " + producto);
+// }
+// function enviar(){
+//     let asunto = document.getElementById("txt-asunto").values;
+//     console.log("asunto " + asunto);
+// }
